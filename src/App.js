dcaf7912f3f9b2514e9aee2a03c650e63fbf0e80
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-07-10 10:12:10 -0400

    Moved to components folder

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import "./App.css";
-
-function App(props) {
-  return <div>App</div>;
-}
-
-export default App;
