080e5ada71957f3f3f8a854cc03c081996275a1e
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-07-11 10:50:29 -0400

    Added functionality to handle user search and display a list of filtered companies

--- src/components/CompanyList/CompanyList.js
+++ src/components/CompanyList/CompanyList.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CompanyCard from "../CompanyCard/CompanyCard";
+import JoblyApi from "../../api";
 
 const CompanyList = () => {
-  return <div>CompanyList</div>;
+  const [companies, setCompanies] = useState([]);
+  const [formData, setFormData] = useState({ searchValue: "" });
+
+  const handleSearch = (e) => {
+    const { value } = e.target;
+    setFormData({ searchValue: value });
+  };
+
+  // fetch list of all companies or filtered companies based on input
+  useEffect(() => {
+    const getFilteredCompanies = async () => {
+      const res = await JoblyApi.getFilteredCompanies(formData.searchValue);
+      setCompanies(res);
+    };
+    getFilteredCompanies();
+  }, [formData]);
+
+  return (
+    <div className="CompanyList">
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="search">Search for Companies</label>
+        <input
+          type="search"
+          id="search"
+          name="search"
+          value={formData.searchValue || ""}
+          onChange={handleSearch}
+        />
+      </form>
+      {companies
+        ? companies.map((company) => (
+            <CompanyCard company={company} key={company.handle} />
+          ))
+        : null}
+    </div>
+  );
 };
 
 export default CompanyList;
