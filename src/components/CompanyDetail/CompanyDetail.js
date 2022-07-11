c21f1c1a51edfa27cf3de10695ce18ce9605d674
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-07-11 14:08:59 -0400

    Updated to show jobs for particular company

--- src/components/CompanyDetail/CompanyDetail.js
+++ src/components/CompanyDetail/CompanyDetail.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import JoblyApi from "../../api";
+import Card from "../Card/Card";
 
 const CompanyDetail = () => {
-  return <div>CompanyDetail</div>;
+  const [companyJobs, setCompanyJobs] = useState(null);
+  const { handle } = useParams();
+  useEffect(() => {
+    const getJobs = async () => {
+      const res = await JoblyApi.getCompany(handle);
+      setCompanyJobs(res.jobs);
+    };
+    getJobs();
+  }, [handle]);
+
+  return (
+    <div className="CompanyDetail">
+      {companyJobs
+        ? companyJobs.map((job) => <Card data={job} type="job" key={job.id} />)
+        : null}
+    </div>
+  );
 };
 
 export default CompanyDetail;
