b474f50d88ee68ffd3f4028284d4eca2eb2c0041
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-07-15 09:52:16 -0400

    Added login method and removed hardcoded token

--- src/api.js
+++ src/api.js
@@ -56,12 +56,14 @@ class JoblyApi {
     const res = await this.request(`jobs`, query);
     return res.jobs;
   }
-}
 
-// for now, put token ("testuser" / "password" on class)
-JoblyApi.token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
-  "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
-  "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
+  /** Login user */
+
+  static async login(data) {
+    const res = await this.request(`auth/token`, data, "post");
+    return res;
+  }
+}
+JoblyApi.token = "";
 
 export default JoblyApi;
