2ddc1a3bd6311a289e8c70783e362c8653c329a9
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-07-15 15:36:37 -0400

    Added UserContext, create logout function, and protect routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AllRoutes from "./AllRoutes";
 import "./App.css";
 
-function App(props) {
-  return (
-    <>
-      <AllRoutes />
-    </>
-  );
-}
+const App = () => {
+  return <AllRoutes />;
+};
 
 export default App;
