5bf86ef5e12bd24e5b9b788d2e0b7beb1154e6c0
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-07-15 09:53:59 -0400

    Added Login component to display login form and login user

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ login }) => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({ username: "", password: "" });
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(() => ({ ...formData, [name]: value }));
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Submitted");
+    const res = await login(formData);
+    if (res.success) {
+      console.log(res);
+      navigate("/", { replace: true });
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          value={formData.username}
+          onChange={handleInputChange}
+          id="username"
+          name="username"
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="text"
+          value={formData.password}
+          onChange={handleInputChange}
+          id="password"
+          name="password"
+        />
+      </div>
+      <button>Login</button>
+    </form>
+  );
 };
 
 export default Login;
