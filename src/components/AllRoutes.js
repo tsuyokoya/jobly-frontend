fbd18fd8a962a4e5764a938f6ffd7b543e0c332b
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-07-15 09:53:30 -0400

    Added state for token and user, login function, and useEffect to fetch user data

--- src/components/AllRoutes.js
+++ src/components/AllRoutes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Homepage from "./Homepage/Homepage";
 import CompanyList from "./CompanyList/CompanyList";
@@ -8,8 +8,26 @@ import Login from "./Login/Login";
 import Signup from "./Signup/Signup";
 import Profile from "./Profile/Profile";
 import Navbar from "./Navbar/Navbar";
+import JoblyApi from "../api";
+import { decodeToken } from "react-jwt";
 
 const AllRoutes = () => {
+  const [currentUser, setCurrentUser] = useState("");
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    JoblyApi.token = token;
+    console.log(token);
+    const user = decodeToken(token);
+    setCurrentUser(user);
+  }, [token]);
+
+  const login = async (data) => {
+    const res = await JoblyApi.login(data);
+    setToken(res.token);
+    return { success: true };
+  };
+
   return (
     <BrowserRouter>
       <Navbar />
@@ -22,7 +40,7 @@ const AllRoutes = () => {
           element={<CompanyDetail />}
         ></Route>
         <Route exact path="/jobs" element={<JobList />}></Route>
-        <Route exact path="/login" element={<Login />}></Route>
+        <Route exact path="/login" element={<Login login={login} />}></Route>
         <Route exact path="/signup" element={<Signup />}></Route>
         <Route exact path="/profile" element={<Profile />}></Route>
         <Route path="*" element={<Navigate to="/colors" replace />}></Route>
