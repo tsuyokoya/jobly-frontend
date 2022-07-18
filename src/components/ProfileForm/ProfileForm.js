b2a20c207adabedfc30a3095a8f160ba3bcbfc67
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-07-17 20:51:22 -0400

    Created ProfileForm component to allow user updates to user data

---
+++ src/components/ProfileForm/ProfileForm.js
@@ -0,0 +1,73 @@
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import UserContext from "../UserContext";
+
+const ProfileForm = ({ updateUserData }) => {
+  const { currentUser } = useContext(UserContext);
+  const INITIAL_STATE = {
+    firstName: currentUser.firstName,
+    lastName: currentUser.lastName,
+    email: currentUser.email,
+  };
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState(INITIAL_STATE);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(() => ({ ...formData, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    for (let fieldValue of Object.values(formData)) {
+      if (fieldValue === "") {
+        return alert("Please fill out each field!");
+      }
+    }
+    const res = await updateUserData(formData, currentUser.username);
+    if (res.success) {
+      navigate("/", { replace: true });
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <p>{currentUser.username}</p>
+      </div>
+      <div>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          value={formData.firstName}
+          onChange={handleInputChange}
+          id="firstName"
+          name="firstName"
+        />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          value={formData.lastName}
+          onChange={handleInputChange}
+          id="lastName"
+          name="lastName"
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          value={formData.email}
+          onChange={handleInputChange}
+          id="email"
+          name="email"
+        />
+      </div>
+      <button>Register</button>
+    </form>
+  );
+};
+
+export default ProfileForm;
