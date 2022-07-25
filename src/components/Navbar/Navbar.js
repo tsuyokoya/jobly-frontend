64aff26bd39f526d9c594af7d66d1128b6e4e045
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-07-25 12:29:06 -0400

    Updated link order

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-const Navbar = () => {
-  return (
-    <>
+const Navbar = ({ logout, token }) => {
+  return token === null ? (
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/login">Login</NavLink>
+      <NavLink to="/signup">Signup</NavLink>
+    </nav>
+  ) : (
+    <nav>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/companies">Companies</NavLink>
       <NavLink to="/jobs">Jobs</NavLink>
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/signup">Signup</NavLink>
       <NavLink to="/profile">Profile</NavLink>
-    </>
+      <NavLink to="/" onClick={logout}>
+        Logout
+      </NavLink>
+    </nav>
   );
 };
 
