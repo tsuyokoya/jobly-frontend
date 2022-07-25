2fc19a38a1cbc0c790fc5a03f3199e65df8be3e0
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-07-25 12:27:53 -0400

    Update homepage welcome message

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Homepage = () => {
-  return <div>Homepage</div>;
+const Homepage = ({ user }) => {
+  const welcomeMsg = user ? (
+    <p>Welcome back, {user.firstName}</p>
+  ) : (
+    <p>Log In or Sign Up to Use!</p>
+  );
+  return (
+    <div>
+      <h1>Jobly</h1>
+      <p>All the fake jobs in one, convenient place.</p>
+      {welcomeMsg}
+    </div>
+  );
 };
 
 export default Homepage;
