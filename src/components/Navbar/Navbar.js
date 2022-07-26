b9115e426cbaf4672801ad1cb873eff56c6284cb
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-07-26 11:24:00 -0400

    Added reactstrap and general css styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,78 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { Navbar as ReactstrapNavbar, Nav, NavItem } from "reactstrap";
+import "./Navbar.css";
 
 const Navbar = ({ logout, token }) => {
   return token === null ? (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/signup">Signup</NavLink>
-    </nav>
+    <ReactstrapNavbar className="fixed-top" color="light">
+      <Nav>
+        <NavItem className="p-1">
+          <NavLink className="nav-link text-decoration-none text-dark" to="/">
+            Home
+          </NavLink>
+        </NavItem>
+        <NavItem className="p-1">
+          <NavLink
+            className="nav-link text-decoration-none text-dark"
+            to="/login"
+          >
+            Login
+          </NavLink>
+        </NavItem>
+        <NavItem className="p-1">
+          <NavLink
+            className="nav-link text-decoration-none text-dark"
+            to="/signup"
+          >
+            Signup
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </ReactstrapNavbar>
   ) : (
-    <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/companies">Companies</NavLink>
-      <NavLink to="/jobs">Jobs</NavLink>
-      <NavLink to="/profile">Profile</NavLink>
-      <NavLink to="/" onClick={logout}>
-        Logout
-      </NavLink>
-    </nav>
+    <ReactstrapNavbar className="fixed-top" color="light">
+      <Nav>
+        <NavItem className="p-1">
+          <NavLink className="nav-link text-decoration-none text-dark" to="/">
+            Home
+          </NavLink>
+        </NavItem>
+        <NavItem className="p-1">
+          <NavLink
+            className="nav-link text-decoration-none text-dark"
+            to="/companies"
+          >
+            Companies
+          </NavLink>
+        </NavItem>
+        <NavItem className="p-1">
+          <NavLink
+            className="nav-link text-decoration-none text-dark"
+            to="/jobs"
+          >
+            Jobs
+          </NavLink>
+        </NavItem>
+        <NavItem className="p-1">
+          <NavLink
+            className="nav-link text-decoration-none text-dark"
+            to="/profile"
+          >
+            Profile
+          </NavLink>
+        </NavItem>
+        <NavItem className="p-1">
+          <NavLink
+            className="nav-link text-decoration-none text-dark"
+            to="/logout"
+            onClick={logout}
+          >
+            Logout
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </ReactstrapNavbar>
   );
 };
 
