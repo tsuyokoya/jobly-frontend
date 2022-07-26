04f905bd46f9615b34b34cdd89afa7cb0f8803e5
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-07-26 11:23:11 -0400

    Added reactstrap styling and prevent submission of empty form fields

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Form, FormGroup, Input, Label, Button } from "reactstrap";
 
 const Login = ({ login }) => {
   const navigate = useNavigate();
@@ -10,35 +11,46 @@ const Login = ({ login }) => {
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
+    for (let fieldValue of Object.values(formData)) {
+      if (fieldValue === "") {
+        return alert("Please fill out each field!");
+      }
+    }
     const res = await login(formData);
     if (res.success) {
       navigate("/", { replace: true });
     }
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">Username</label>
-        <input
+    <Form
+      className="Login h5 d-flex flex-column align-items-center"
+      onSubmit={handleSubmit}
+      style={{ marginTop: "100px" }}
+    >
+      <FormGroup className="w-50">
+        <Label htmlFor="username">Username</Label>
+        <Input
           type="text"
           value={formData.username}
           onChange={handleInputChange}
           id="username"
           name="username"
         />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="text"
+      </FormGroup>
+      <FormGroup className="w-50">
+        <Label htmlFor="password">Password</Label>
+        <Input
+          type="password"
           value={formData.password}
           onChange={handleInputChange}
           id="password"
           name="password"
         />
-      </div>
-      <button>Login</button>
-    </form>
+      </FormGroup>
+      <Button className="w-50 mt-2" color="primary" outline>
+        Login
+      </Button>
+    </Form>
   );
 };
 
