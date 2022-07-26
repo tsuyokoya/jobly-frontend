c48e0aee34f3e8d1e70a948c3d35e035d304168e
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-07-26 11:21:09 -0400

    Added companyName state, handleApply function, and reactstrap styling

--- src/components/CompanyDetail/CompanyDetail.js
+++ src/components/CompanyDetail/CompanyDetail.js
@@ -5,23 +5,35 @@ import Card from "../Card/Card";
 
 const CompanyDetail = ({ currentUser }) => {
   const [companyJobs, setCompanyJobs] = useState(null);
+  const [companyName, setCompanyName] = useState(null);
   const { handle } = useParams();
+
   useEffect(() => {
     const getJobs = async () => {
       const res = await JoblyApi.getCompany(handle);
       setCompanyJobs(res.jobs);
+      setCompanyName(res.name);
     };
     getJobs();
   }, [handle]);
 
+  const handleApply = async (e, id) => {
+    return await JoblyApi.applyToJob(id, currentUser.username);
+  };
+
   return (
-    <div className="CompanyDetail">
+    <div
+      className="CompanyDetail d-flex flex-column align-items-center"
+      style={{ marginTop: "100px" }}
+    >
+      <h1>Jobs at {companyName}</h1>
       {companyJobs
         ? companyJobs.map((job) => (
             <Card
               data={job}
               type="job"
               currentUser={currentUser}
+              handleApply={handleApply}
               key={job.id}
             />
           ))
