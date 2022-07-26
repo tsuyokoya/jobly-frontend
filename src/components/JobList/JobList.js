5330021cf0d4656d55c3ecfa7d2cdf41d6ca46d2
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-07-26 11:22:34 -0400

    Added reactstrap styling and Loading text

--- src/components/JobList/JobList.js
+++ src/components/JobList/JobList.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Card from "../Card/Card";
 import JoblyApi from "../../api";
+import { Form, Input, Label } from "reactstrap";
 
 const JobList = ({ currentUser }) => {
   const [jobs, setJobs] = useState([]);
   const [formData, setFormData] = useState({ searchValue: "" });
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleSearch = (e) => {
     const { value } = e.target;
@@ -17,35 +19,53 @@ const JobList = ({ currentUser }) => {
       const res = await JoblyApi.getFilteredJobs(formData.searchValue);
       setJobs(res);
     };
-    getFilteredJobs();
+    getFilteredJobs().then(() => {
+      setIsLoading(false);
+    });
   }, [formData]);
 
   const handleApply = async (e, id) => {
     return await JoblyApi.applyToJob(id, currentUser.username);
   };
 
+  if (isLoading) {
+    return (
+      <div className="d-flex flex-column align-items-center justify-content-center vh-100">
+        <h1 className="display-4">Loading...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div className="JobList">
-      <form onSubmit={(e) => e.preventDefault()}>
-        <label htmlFor="search-jobs">Search for Jobs</label>
-        <input
+    <div className="JobList d-flex flex-column align-items-center">
+      <Form
+        className="d-flex flex-row w-50"
+        style={{ marginTop: "100px" }}
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <Label hidden htmlFor="search-jobs">
+          Search jobs:
+        </Label>
+        <Input
           type="search"
           id="search-jobs"
           value={formData.searchValue || ""}
           onChange={handleSearch}
+          placeholder="Search for a job"
+          bsSize="lg"
         />
-      </form>
-      {jobs
-        ? jobs.map((job) => (
-            <Card
-              data={job}
-              handleApply={handleApply}
-              currentUser={currentUser}
-              type="job"
-              key={job.id}
-            />
-          ))
-        : null}
+      </Form>
+      {jobs.map((job) => {
+        return (
+          <Card
+            data={job}
+            handleApply={handleApply}
+            currentUser={currentUser}
+            type="job"
+            key={job.id}
+          />
+        );
+      })}
     </div>
   );
 };
